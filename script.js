33f0e4b373347ef20f046c0bed0d753790b1c058
Zuby Javed <zuby@Zubys-MacBook-Pro.local>
2022-03-13 14:49:49 -0700

    remove spaghetti code with less spaghetti code

--- script.js
+++ script.js
@@ -1,73 +1,68 @@
 /* Pics to be hidden/displayed. */
-let icecube = document.getElementById('icecube');
-let smh = document.getElementById('smh');
-let obama = document.getElementById('obama');
-let idubbz = document.getElementById('idubbz');
-let arnold = document.getElementById('arnold');
+// let icecube = document.getElementById('icecube');
+// let smh = document.getElementById('smh');
+// let obama = document.getElementById('obama');
+// let idubbz = document.getElementById('idubbz');
+// let arnold = document.getElementById('arnold');
 
-arnold.style.display = "block";
-
-/* Shows the picture corresponding to tip option */
-function displayPic() {
-    //Default tip amount
-    let value = ".25";
-
-    value = document.getElementById('tip').value;
-
+/* Shows pic corresponding to tip option */
+function displayImg() {
+    let select = document.getElementById("tip");
+    let value = select.options[select.selectedIndex].value;
+    let name = '';
+    
     switch(value) {
-        case "0.1":
-
+        case '0' :
+            name = 'food_guy.jpeg';
+            break;
+        case '.01' :
+            name = 'smh.jpeg';
+            break;
+        case '.05' :
+            name = 'icecube.jpg';
+            break;
+        case '.1' :
+            name = 'obama.jpg';
+            break;
+        case '.15':
+            name = 'arnold.jpg';
+            break;
+        case '.2' :
+            name = 'doge.jpeg';
+            break;
     }
 
-    if (value === "0.1") {
-        icecube.style.display = "block";
-    }
-    else if (value === ".10") {
-        smh.style.display = "block";
-    }
-    else if (value === ".15") {
-        obama.style.display = "block";
-    }
-    else if (value === ".20") {
-        idubbz.style.display = "block";
-    }
-    else {
-        arnold.style.display = "block";
-    }
+    var pic = document.getElementById("pic"); 
+    var imgSrc = 'images/' + name;
+    pic.setAttribute('src', imgSrc);
 }
-
-/* Hides all pictures from displaying */
-function hidePics() {
-    icecube.style.display = "none";
-    smh.style.display = "none";
-    obama.style.display = "none";
-    idubbz.style.display = "none";
-    arnold.style.display = "none";
-}
-
 // Calculates the total price of the meal and price per person.
 function calculate() {
-    const price =  document.getElementById('total').value;
+
+    const totalDisplay = document.getElementById('totalDisplay'); 
+    const splitTotal = document.getElementById('splitTotal');
+    
+    // clears previous total
+    totalDisplay.innerHTML = '';
+    splitTotal.innerHTML = '';
+
+    const price = document.getElementById('total').value;
     const tip =  document.getElementById('tip').value;
-    const numPeople =  document.getElementById('people').value;
+    const numPeople = document.getElementById('people').value;
 
     // Computes total price and price per person.
     let totalPrice = +(price * (tip)) + +price;
     let pricePerPerson =  totalPrice / numPeople;
+
     // Rounds results to two decimal palces
     totalPrice = totalPrice.toFixed(2);
     pricePerPerson = pricePerPerson.toFixed(2);
 
-    console.log('total price ' + totalPrice);
-
-    //Creates text output.
+    // Creates text output.
     totalPrice = document.createTextNode(totalPrice);
     pricePerPerson = document.createTextNode( pricePerPerson);
 
-    // Adds text output to hidden div.
-    document.getElementById('totalDisplay').appendChild(totalPrice);
-    document.getElementById('splitTotal').appendChild(pricePerPerson);
-
-    // Displays hidden div with output.
-    document.getElementById('finalAmount').style.display = 'block';
+    // Displays text output to user.
+    totalDisplay.appendChild(totalPrice);
+    splitTotal.appendChild(pricePerPerson);
 }
